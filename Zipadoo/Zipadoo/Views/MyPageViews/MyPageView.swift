ff11390e57df62a18ee8a1c8e599da65ae9f8dc3
JASONLEE-hub <seoung128@gmail.com>
2023-09-21 17:59:57 +0900

    [FEAT] 마이페이지뷰 메인 하드코딩 완료

--- Zipadoo/Zipadoo/Views/MyPageViews/MyPageView.swift
+++ Zipadoo/Zipadoo/Views/MyPageViews/MyPageView.swift
@@ -8,8 +8,135 @@
 import SwiftUI
 
 struct MyPageView: View {
+    let dummyImageString: String = "https://cdn.discordapp.com/attachments/1154228426660642846/1154313075453866044/abde4177d5e5093a.png"
+    
+    let dummyKm: Int = 1000
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    //프로필 이미지
+                    AsyncImage(url: URL(string: dummyImageString)) { image in
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    Spacer()
+                    //프로필 기능모음
+                    VStack(alignment: .trailing) {
+                        NavigationLink {
+                            MyPotatoView()
+                        } label: {
+                            Image(systemName: "gearshape")
+                                .foregroundColor(.black)
+                        }
+                        .padding(.bottom)
+                        Text("이재승 님")
+                            .padding(.bottom)
+                        HStack {
+                            Image(systemName: "bitcoinsign.circle.fill")
+                            Text("10,000")
+                                .font(.title2)
+                                .bold()
+                        }
+                        NavigationLink {
+                            PayingView()
+                        } label: {
+                            Text("충전하기")
+                                .font(.title2)
+                        }
+                    }
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                
+                Divider()
+                
+                HStack {
+                    Text("지각 깊이")
+                        .font(.title2)
+                    Spacer()
+                    Text("지하 \(dummyKm)km")
+                }
+                .padding(.top)
+                
+                Rectangle()
+                    .frame(width: .infinity, height: 30)
+                    .foregroundColor(.brown)
+                    .cornerRadius(20)
+                
+                HStack {
+                    Text("이대로 가다간,, 친구들한테 파묻히겠어요 ㅠ")
+                    Spacer()
+                    Text("지각률 80%")
+                        .foregroundColor(.red)
+                }
+                .padding(.top)
+                .padding(.bottom)
+                
+                Divider()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack {
+                        Text("나의 약속")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    .font(.title2)
+                    .foregroundColor(.black)
+                    .padding(.top)
+                    .padding(.bottom)
+                }
+                
+                Divider()
+                
+                Text("받은 매너 평가")
+                    .font(.title2)
+                    .padding(.top)
+                
+                Group {
+                    
+                    HStack {
+                        Image(systemName: "person.2")
+                        Text("4")
+                        Text("약속 진짜 안지켜요")
+                            .padding(10)
+                            .background(.gray)
+                            .cornerRadius(80)
+                            .opacity(0.5)
+                    }
+                    
+                    HStack {
+                        Image(systemName: "person.2")
+                        Text("3")
+                        Text("진짜 지각만해서 지각에 묻어버리고 싶어요")
+                            .padding(10)
+                            .background(.gray)
+                            .cornerRadius(80)
+                            .opacity(0.5)
+                    }
+                    
+                    HStack {
+                        Image(systemName: "person.2")
+                        Text("12")
+                        Text("한 20분 정도 늦어요")
+                            .padding(10)
+                            .background(.gray)
+                            .cornerRadius(80)
+                            .opacity(0.5)
+                    }
+                    
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
