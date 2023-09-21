c61a24f81567f56a1507d66c4b5740339fe96049
Tag <sea15510@gmail.com>
2023-09-21 16:38:28 +0900

    [FEAT] TabView Add
    -

--- Zipadoo/Zipadoo/Views/ContentView.swift
+++ Zipadoo/Zipadoo/Views/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            // 홈
+            Text("홈 뷰")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("홈")
+                }
+                .tag(0)
+            
+            // 친구리스트
+            Text("친구리스트 뷰")
+                .tabItem {
+                    Image(systemName: "person.2.fill")
+                    Text("친구")
+                }
+                .tag(1)
+            
+            // 마이페이지
+            Text("마이페이지 뷰")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("마이")
+                }
+                .tag(1)
         }
-        .padding()
     }
 }
 
