936ebcb850865a81452fe9fd29ce4deb0013dc61
Tag <sea15510@gmail.com>
2023-09-21 10:34:26 +0900

    Feat: Zipadoo 프로젝트 추가
    -

---
+++ Zipadoo/Zipadoo/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zipadoo
+//
+//  Created by 윤해수 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zipadoo/Zipadoo/ZipadooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipadooApp.swift
+//  Zipadoo
+//
+//  Created by 윤해수 on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ZipadooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
