fc36a89e8d14a376e213056ba532dd5a1413c625
JASONLEE-hub <seoung128@gmail.com>
2023-09-21 15:50:36 +0900

    [FEAT] 마이페이지뷰 생성 #7

---
+++ Zipadoo/Zipadoo/Views/MyPageViews/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Zipadoo
+//
+//  Created by 이재승 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
---
+++ Zipadoo/Zipadoo/Views/MyPageViews/PayingView.swift
@@ -0,0 +1,18 @@
+//
+//  PayingView.swift
+//  Zipadoo
+//
+//  Created by 이재승 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct PayingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PayingView()
+}
