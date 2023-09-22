42d4def006b5c967b9f49665ba53bb557f9b7322
jangyeohoon <someday0307@naver.com>
2023-09-22 10:07:17 +0900

    [Feat] FriendsLocationStatusView
    뷰 파일 생성

---
+++ Zipadoo/Zipadoo/Views/Home/FriendsLocationStatusView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsLocationStatus.swift
+//  Zipadoo
+//
+//  Created by 장여훈 on 2023/09/22.
+//
+
+import SwiftUI
+
+struct FriendsLocationStatusView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendsLocationStatusView()
+}
