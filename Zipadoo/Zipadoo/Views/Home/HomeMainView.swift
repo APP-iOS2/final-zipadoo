b0919b046291c5709b3dc4106b49b0f23b5d141d
Tag <sea15510@gmail.com>
2023-09-22 11:08:42 +0900

    [FEAT] 약속 리스트뷰 구현 #11
    파이어베이스 연동 추후 작업
    
    -

--- Zipadoo/Zipadoo/Views/Home/HomeMainView.swift
+++ Zipadoo/Zipadoo/Views/Home/HomeMainView.swift
@@ -18,38 +18,48 @@ struct HomeMainView: View {
             if dataArray.isEmpty {
                 Text("현재 약속이 1도 없어요!")
             } else {
-                List(dataArray, id: \.self) { promise in
-                    NavigationLink {
-                        // 각 약속 상세 뷰로 이동
-                    } label: {
-                        VStack(alignment: .leading) {
-                            HStack {
-                                Text(promise.promiseTitle)
-                                    .font(.title)
+                ScrollView {
+                    ForEach(dataArray, id: \.self) { promise in
+                        NavigationLink {
+                            // 각 약속 상세 뷰로 이동
+                        } label: {
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text(promise.promiseTitle)
+                                        .font(.title)
+                                    
+                                    Spacer()
+                                    
+                                    Text("위치 공유 중")
+                                        .foregroundStyle(Color.white)
+                                        .padding(.horizontal, 12)
+                                        .padding(.vertical, 5)
+                                        .background(Color.gray)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.black, lineWidth: 1)
+                                        )
+                                }
                                 
-                                Spacer()
+                                Text("약속 장소 : \(promise.destination)")
                                 
-                                Text("위치 공유 중")
-                                    .padding(8)
-                                    .background(Color.blue)
-                                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                                    .overlay(
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(Color.black, lineWidth: 1)
-                                    )
+                                /// 저장된 promiseDate값을 Date 타입으로 변환
+                                let datePromise = Date(timeIntervalSince1970: promise.promiseDate)
+                                Text("약속 시간 : \(promise.promiseDate)")
+                                
+                                // 참여자의 ID를 통해 참여자 정보 가져오기
                             }
-                            
-                            Text("약속 장소 : \(promise.destination)")
-                            
-                            /// 저장된 promiseDate값을 Date 타입으로 변환
-                            let datePromise = Date(timeIntervalSince1970: promise.promiseDate)
-                            Text("약속 시간 : \(promise.promiseDate)")
-                            
-                            // 참여자의 ID를 통해 참여자 정보 가져오기
+                            .padding()
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                            .foregroundStyle(Color.black)
                         }
                     }
                 }
-                .listStyle(.sidebar)
+                .padding()
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
                         NavigationLink {
