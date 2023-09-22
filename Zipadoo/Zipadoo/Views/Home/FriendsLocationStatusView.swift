19d9bfe951258a7afae20ec5ffc1c0397c163937
jangyeohoon <someday0307@naver.com>
2023-09-22 14:51:11 +0900

    [Feat] FriendsLocationStatusView
    이미지, 애니메이션 구현
    참가자 리스트에 적용 예정

--- Zipadoo/Zipadoo/Views/Home/FriendsLocationStatusView.swift
+++ Zipadoo/Zipadoo/Views/Home/FriendsLocationStatusView.swift
@@ -8,8 +8,85 @@
 import SwiftUI
 
 struct FriendsLocationStatusView: View {
+    @State private var value: Double = 0.9
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ProgressWithImageView(value: value, label: { Text("Test") }, currentValueLabel: { Text("\(Int(value*100)) %") })
+                .progressViewStyle(BarProgressStyle(height: 50))
+                .transition(.opacity)
+                .animation(.easeInOut(duration: 0.5))
+                .shadow(radius: 5)
+            
+            HStack {
+                Button(action: {
+                    if self.value >= 0.05 {
+                        self.value -= 0.05
+                    }
+                }, label: {
+                    Text("-")
+                        .font(.title)
+                })
+                
+                Button(action: {
+                    if self.value <= 0.99 {
+                        self.value += 0.05
+                    }
+                }, label: {
+                    Text("+")
+                        .font(.title)
+                })
+            }
+        }
+        .padding()
+    }
+}
+
+struct BarProgressStyle: ProgressViewStyle {
+    var color: UIColor = #colorLiteral(red: 0.4964652658, green: 0.2767619491, blue: 0.01609945111, alpha: 1)
+    var height: Double = 20.0
+    var labelFontStyle: Font = .body
+    
+    func makeBody(configuration: Configuration) -> some View {
+        let progress = configuration.fractionCompleted ?? 0.0
+        
+        return GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                configuration.label.font(labelFontStyle)
+                
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color(uiColor: .systemGray5))
+                        .frame(height: height)
+                        .frame(width: geometry.size.width)
+                    
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .fill(Color(color))
+                        .frame(width: geometry.size.width * CGFloat(progress), height: height)
+                        .overlay {
+                            if let currentValueLabel = configuration.currentValueLabel {
+                                currentValueLabel
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    Image("MoleImage")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .offset(x: geometry.size.width * CGFloat(progress) - 25, y: 0)
+                }
+            }
+        }
+    }
+}
+
+struct ProgressWithImageView: View {
+    var value: Double
+    var label: () -> Text
+    var currentValueLabel: () -> Text
+    
+    var body: some View {
+        ProgressView(value: value, label: label, currentValueLabel: currentValueLabel)
     }
 }
 
