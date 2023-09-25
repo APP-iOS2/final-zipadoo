7a474a6296d74207b3de2e0d6e99271129f6a22a
JASONLEE-hub <seoung128@gmail.com>
2023-09-25 15:04:53 +0900

    [FEAT] 카카오로그인 기능

---
+++ Zipadoo/Zipadoo/ViewModels/KakaoStore.swift
@@ -0,0 +1,8 @@
+//
+//  KakaoStore.swift
+//  Zipadoo
+//
+//  Created by 이재승 on 2023/09/25.
+//
+
+import Foundation
---
+++ Zipadoo/Zipadoo/Views/KakaoTest.swift
@@ -0,0 +1,18 @@
+//
+//  kakaoTest.swift
+//  Zipadoo
+//
+//  Created by 이재승 on 2023/09/23.
+//
+
+import SwiftUI
+
+struct kakaoTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    kakaoTest()
+}
