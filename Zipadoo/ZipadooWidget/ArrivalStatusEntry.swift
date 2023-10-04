80f461228109e2218442e2a80fe43e180604bfdc
SunAra25 <tjsdkfk77@naver.com>
2023-10-04 15:15:19 +0900

    [FEAT] 약속 정보 entry 및 도착 현황 entry 구현 #58

---
+++ Zipadoo/ZipadooWidget/ArrivalStatusEntry.swift
@@ -0,0 +1,14 @@
+//
+//  ArrivalStatusEntry.swift
+//  Zipadoo
+//
+//  Created by 아라 on 10/4/23.
+//
+
+import WidgetKit
+
+struct ArrivalStatusEntry: TimelineEntry {
+    let date: Date
+    let name: String
+    let percentage: Double
+}
