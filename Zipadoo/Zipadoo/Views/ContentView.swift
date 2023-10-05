5c4ff0b2b28004d5130e34cb5e780600e2613b2d
Tag <sea15510@gmail.com>
2023-10-05 09:08:50 +0900

    Revert "Dev/mypage"

--- Zipadoo/Zipadoo/Views/ContentView.swift
+++ Zipadoo/Zipadoo/Views/ContentView.swift
@@ -8,42 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @StateObject var viewModel = ContentViewModel()
-    
     var body: some View {
-        
-        // 로그인이 되어있지 않은 상태면 로그인 뷰로,
-        if viewModel.userSession == nil {
-            LoginView()
-        } else {
-            TabView {
-                // 홈
-                HomeMainView()
-                    .tabItem {
-                        Image(systemName: "house.fill")
-                        Text("홈")
-                    }
-                    .tag(0)
-                
-                // 친구리스트
-                FriendsView()
-                    .tabItem {
-                        Image(systemName: "person.2.fill")
-                        Text("친구")
-                    }
-                    .tag(1)
-                
-                // 마이페이지
-                MyPageView()
-                    .tabItem {
-                        Image(systemName: "person.fill")
-                        Text("마이")
-                    }
-                    .tag(2)
-                
-            }
+        VStack {
+            Text("로그인뷰^ㅡ^")
         }
+        .padding()
     }
 }
 
