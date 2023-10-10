c6cd9dad67aa43aa3fa055959d560bf1bd1631c1
9oos <b_9@kakao.com>
2023-10-10 23:41:02 +0900

    [FIX] error 로 인해 recommit

--- Zipadoo/Zipadoo/Views/Home/HomeMainView.swift
+++ Zipadoo/Zipadoo/Views/Home/HomeMainView.swift
@@ -95,7 +95,7 @@ struct HomeMainView: View {
                                         ZStack {
                                             RoundedRectangle(cornerRadius: 10)
                                                 .opacity(0.2)
-                                                .shadow(color: .primary, radius: 15, x: 10, y: 10)
+                                                .shadow(color: .secondary, radius: 10, x: 5, y: 5)
                                                 
                                             //                                        .stroke(Color.black, lineWidth: 0.3)
                                             
@@ -129,6 +129,36 @@ struct HomeMainView: View {
                         })
                     }
                 } // toolbar
+                .onAppear {
+                                  print(Date().timeIntervalSince1970)
+                                  var calendar = Calendar.current
+                                  calendar.timeZone = NSTimeZone.local
+                                  let encoder = JSONEncoder()
+
+                                  var widgetDatas: [WidgetData] = []
+
+                                  for promise in promise.promiseViewModel {
+                                      let promiseDate = Date(timeIntervalSince1970: promise.promiseDate)
+                                      let promiseDateComponents = calendar.dateComponents([.year, .month, .day], from: promiseDate)
+                                      let todayComponents = calendar.dateComponents([.year, .month, .day], from: Date())
+
+                                      if promiseDateComponents == todayComponents {
+                                          // TODO: 도착 인원 수 파베 연동 후 테스트하기. 지금은 0으로!
+                                          let data = WidgetData(title: promise.promiseTitle, time: promise.promiseDate, place: promise.destination, arrivalMember: 0)
+                                          widgetDatas.append(data)
+                                      }
+                                  }
+
+                                  do {
+                                      let encodedData = try encoder.encode(widgetDatas)
+
+                                      UserDefaults.shared.set(encodedData, forKey: "todayPromises")
+
+                                      WidgetCenter.shared.reloadTimelines(ofKind: "ZipadooWidget")
+                                  } catch {
+                                      print("Failed to encode Promise:", error)
+                                  }
+                              }
             }  // ScrollView
            
                     //            .ignoresSafeArea(.all)
