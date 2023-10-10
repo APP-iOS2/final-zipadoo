7a2ae7e8476c26cd517c0c90378cfd51f0a46ad2
SunAra25 <tjsdkfk77@naver.com>
2023-10-10 15:04:51 +0900

    [FEAT] push 알림 세팅 #156

--- Zipadoo/Zipadoo/ZipadooApp.swift
+++ Zipadoo/Zipadoo/ZipadooApp.swift
@@ -6,18 +6,41 @@
 //
 
 import FirebaseCore
+import FirebaseMessaging
 import SwiftUI
+//import UserNotifications
+
 // import KakaoSDKAuth
 // import KakaoSDKCommon
 // import KakaoSDKUser
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    let gcmMessageIDKey = "gcm.message_id"
+    
     // 파이어베이스 초기화
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOption: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOption,
+            completionHandler: {_, _ in })
+        
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+                
+        UNUserNotificationCenter.current().delegate = self
+        
         print("파이어베이스 초기화 할거야!!!!!")
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
     }
     
     /*
@@ -54,4 +77,60 @@ struct ZipadooApp: App {
 //                })
         }
     }
+}
+
+extension AppDelegate: MessagingDelegate {
+    
+    // fcm 등록 토큰을 받았을 때
+    internal func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+
+        print("토큰을 받았다")
+        // Store this token to firebase and retrieve when to send message to someone...
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        
+        // Store token in Firestore For Sending Notifications From Server in Future...
+        
+        print(dataDict)
+     
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 푸시 메세지가 앱이 켜져있을 때 나올떄
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+                                -> Void) {
+        
+        let userInfo = notification.request.content.userInfo
+        
+        
+        // Do Something With MSG Data...
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        
+        print(userInfo)
+        
+        completionHandler([[.banner, .badge, .sound]])
+    }
+    
+    // 푸시메세지를 받았을 떄
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        // Do Something With MSG Data...
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        print(userInfo)
+        
+        completionHandler()
+    }
+    
 }
