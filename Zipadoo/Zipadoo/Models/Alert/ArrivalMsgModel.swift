fa8ff28885b6bd589680084ff3fc4672e8c17fdc
SunAra25 <tjsdkfk77@naver.com>
2023-10-13 17:26:05 +0900

    [FEAT] 약속 장소 도착 Alert Model 구현 #156

---
+++ Zipadoo/Zipadoo/Models/Alert/ArrivalMsgModel.swift
@@ -0,0 +1,17 @@
+//
+//  ArrivalMsgModel.swift
+//  Zipadoo
+//
+//  Created by 아라 on 10/13/23.
+//
+
+import Foundation
+
+struct ArrivalMsgModel {
+    var isPresented: Bool
+    var name: String
+    var profileImgString: String
+    var rank: Int
+    var arrivarDifference: Double
+    var potato: Int
+}
