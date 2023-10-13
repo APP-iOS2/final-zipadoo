ff40724bca9b4760a3fdc3b51d423c54dc217c03
JASONLEE-hub <seoung128@gmail.com>
2023-10-13 13:47:09 +0900

    [FIX] WindowGroup - ContentView()

--- Zipadoo/Zipadoo/ZipadooApp.swift
+++ Zipadoo/Zipadoo/ZipadooApp.swift
@@ -46,7 +46,7 @@ struct ZipadooApp: App {
     
     var body: some Scene {
         WindowGroup {
-            FriendsMapView()
+            ContentView()
 //                .onOpenURL(perform: { url in
 //                    if AuthApi.isKakaoTalkLoginUrl(url) {
 //                        AuthController.handleOpenUrl(url: url)
