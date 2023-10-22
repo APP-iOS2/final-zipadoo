fc282b189bd7fda95ed452b0064d8d8707fc0818
9oos <b_9@kakao.com>
2023-10-23 00:39:28 +0900

    [DESGIN][FEAT] 친구 목록 디자인 수정
    1. friendsView (친구목록 디자인 수정 및 삭제 방식 변경)
      1) 하단 줄 제거, 사진 크기 조정, 글자 크기조정, 칭호 배치
      2) 사진 크기 조정을 위해 프로필사진 사이즈 enum 에mini 사이즈 추가
      3) 친구 리스트에서 삭제 버튼이 계속 보이는는게 적절하지 않다고 생각하여 카카오톡 처럼 swipe 로 삭제가능하게 함.
        (기존처럼 삭제버튼 누룰시 alert 띄워서 삭제하여 실수로 삭제하지 않도록 함 )
    
    2. AddFriendCellView (약속 등록시 하단에 친구 추가 뷰)
      1) 친구 추가 시 선택된 친구는 흐리게 처리 함 (프로필사진은 opcaity, 이름은.secondary)
      2) 테두리 선은 유민영강사님의 지난  피드백에 따라 좀더 얇고 안보이도록 .secondary 줌
    
    3. FriendProfileView
     마이페이지 디자인 반영이 안되있어서 사진 위주로 크게 보여주게 수정하고 나머지 주석처리
    추후 데이터 연동하며 수정 필요

--- Zipadoo/Zipadoo/Views/Friends/FriendsView.swift
+++ Zipadoo/Zipadoo/Views/Friends/FriendsView.swift
@@ -20,6 +20,8 @@ struct FriendsView: View {
     /// 상단 탭바 인덱스
     @State private var tabIndex = 0
     
+    
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -36,6 +38,7 @@ struct FriendsView: View {
                 }
                 Spacer()
             }
+            .fontWeight(.semibold)
             //            .pickerStyle(SegmentedPickerStyle())
             .navigationTitle("친구 관리")
             .navigationBarTitleDisplayMode(.inline)
@@ -44,8 +47,10 @@ struct FriendsView: View {
                     NavigationLink {
                         FriendsRegistrationView(friendsStore: friendsStore)
                     } label: {
-                        Label("Add", systemImage: "person.crop.circle.fill.badge.plus")
+                        Image(systemName:"person.crop.circle.fill.badge.plus")
                     }
+                    .foregroundColor(.primary)
+                    .foregroundStyle(.primary)
                 }
             }
             .alert(isPresented: $isDeleteAlert) {
@@ -87,34 +92,70 @@ struct FriendsView: View {
                     
                 } else {
                     List {
-                        ForEach(friendsStore.friendsFetchArray) { friend in
-                            ZStack {
-                                NavigationLink(destination: FriendProfileView(user: friend), label: {
-                                    HStack {
-                                        ProfileImageView(imageString: friend.profileImageString, size: .xSmall)
-                                        
-                                        Text(friend.nickName)
-                                    }
+                            ForEach(friendsStore.friendsFetchArray) { friend in
+                                ZStack {
+                                NavigationLink(destination: FriendProfileView(user: friend),
+                                               label: {
+                                    // 리스트에 네비게이션 링크 적용시 ">" 마크 제거하기 위하여 ZStack, EmptyView(), Opacatiy(0) 사용
+                                    EmptyView()
                                 })
-                                HStack {
-                                    Spacer()
-                                    
-                                    Text("삭제")
-                                        .padding(5)
-                                        .foregroundColor(.gray)
-                                        .background(.primary)
-                                        .colorInvert()
-                                        .onTapGesture {
-                                            selectedFriendId = friend.id
-                                            isDeleteAlert.toggle()
+                                    .opacity(0)
+
+                                    HStack {
+                                        ProfileImageView(imageString: friend.profileImageString, size: .mini)
+                                        VStack(alignment: .leading) {
+                                            Text(friend.nickName)
+                                                .fontWeight(.semibold)
+                                                .font(.headline)
+                                            //                                            Text(FriendProfileView.user.crustDepth)
+                                            // Text(friend.tardyTitle)
+                                            Text("뉴비약속러")
+                                                .foregroundStyle(Color.secondary)
+                                                .font(.caption)
                                         }
-                                }
-                            }
-                            .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
-                        }
-                    }
+                                        Spacer()
+
+                                    } // Hstack
+                                    
+//                                }// label
+//                                )  // Navi
+                               
+                            } // Foreach
+                                .swipeActions {
+                                    Button {
+                                        selectedFriendId = friend.id
+                                        isDeleteAlert.toggle()
+                                    }label: {
+//                                        Image(systemName: "trash.fill")
+                                        Text("삭제")
+                                            .fontWeight(.semibold)
+                                           
+                                        //                                        .padding(5)
+                                        //                                        .foregroundColor(.secondary)
+                                        //                                        .background(.primaryInvert)
+                                    }
+                                    .tint(.red)
+                                } //swipe
+                            //                                HStack {
+                            //                                    Spacer()
+                            //
+                            //                                    Image(systemName: "")
+                            //                                        .padding(5)
+                            //                                        .foregroundColor(.secondary)
+                            //                                        .background(.primaryInvert)
+                            //                                        .onTapGesture {
+                            //                                            selectedFriendId = friend.id
+                            //                                            isDeleteAlert.toggle()
+                            //                                        }
+                            //                                }
+                            
+                            //                            .padding(.vertical, 5)
+                        } //Zstack
+                        } // List
                     .listStyle(.plain)
-                    
+                   
+                    .padding(.top, -16)
+   
                 }
             }
         }
