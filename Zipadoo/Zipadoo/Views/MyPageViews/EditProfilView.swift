33f8b88e7345aba52d6a27d5d21954fad771cf7c
SunAra25 <tjsdkfk77@naver.com>
2023-10-24 21:10:25 +0900

    [FEAT] 회원 정보 변경 유효성 검사 및 EditProfileViewModel과UserStore 합침 #317

--- Zipadoo/Zipadoo/Views/MyPageViews/EditProfilView.swift
+++ Zipadoo/Zipadoo/Views/MyPageViews/EditProfilView.swift
@@ -10,18 +10,23 @@ import SwiftUI
 // 이메일로 가입되어 있는 사람만 정보 수정 뷰 뜰 것
 struct EditProfileView: View {
     
-    @ObservedObject var viewModel = EditProfileViewModel()
-    
+    //@ObservedObject var viewModel = EditProfileViewModel()
+    @ObservedObject var userStore: UserStore = UserStore()
     @Environment (\.dismiss) private var dismiss
     
+    @State private var nickname: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var selectedImage: UIImage?
     /// 알람노출
     @State private var isEditAlert: Bool = false
     /// 이미지피커 노출
     @State private var isShowingImagePicker = false
+    @State private var nickNameMessage = ""
+    @State private var numberMessage = ""
     
     /// 비어있는 TextField가 있을 때 true
     private var isFieldEmpty: Bool {
-        viewModel.nickname.isEmpty || viewModel.phoneNumber.isEmpty
+        nickname.isEmpty || phoneNumber.isEmpty || !nickNameMessage.isEmpty || !numberMessage.isEmpty
     }
     
     var body: some View {
@@ -34,19 +39,19 @@ struct EditProfileView: View {
                 } label: {
                     // 프로필 사진 선택할 경우 프로필 사진으로 표시, 아닐 경우 파이어베이스에 저장된 이미지 보이도록
                     
-                    if let profileImage = viewModel.selectedImage {
+                    if let profileImage = selectedImage {
                         Image(uiImage: profileImage)
                             .resizable()
                             .scaledToFill()
                             .frame(width: 150, height: 150)
                             .clipShape(Circle())
                     } else {
-                        ProfileImageView(imageString: viewModel.profileImageString, size: .medium)
+                        ProfileImageView(imageString: userStore.getUserProfileImage(), size: .medium)
                     }
                     
                 }
                 .sheet(isPresented: $isShowingImagePicker) {
-                    ImagePicker(selectedImage: $viewModel.selectedImage)
+                    ImagePicker(selectedImage: $selectedImage)
                         .ignoresSafeArea(.all)
                     
                 }
@@ -55,11 +60,40 @@ struct EditProfileView: View {
                 
                 // TextField
                 VStack(alignment: .leading) {
-                    textFieldCell("새로운 닉네임", text: $viewModel.nickname)
+                    textFieldCell("새로운 닉네임", text: $nickname)
                         .padding(.bottom)
+                        .onChange(of: nickname) { newValue in
+                            let filtered = newValue.filter { $0.isLetter || $0.isNumber }
+                            if filtered != newValue {
+                                nickNameMessage = "특수문자는 입력할 수 없습니다."
+                            } else {
+                                nickNameMessage = ""
+                            }
+                        }
                     
-                    textFieldCell("새로운 연락처", text: $viewModel.phoneNumber)
+                    Text(nickNameMessage)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .padding(.top, -15)
+                        .padding(.bottom, 5)
+                        
+                    
+                    textFieldCell("새로운 연락처", text: $phoneNumber)
                         .padding(.bottom)
+                        .onChange(of: phoneNumber) { newValue in
+                            let filtered = newValue.filter { $0.isNumber }
+                            if filtered != newValue {
+                                numberMessage = "숫자만 입력해주세요"
+                            } else {
+                                numberMessage = ""
+                            }
+                        }
+                    
+                    Text(numberMessage)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                        .padding(.top, -15)
+                        .padding(.bottom, 5)
                     
                 }
                 .padding()
@@ -69,6 +103,10 @@ struct EditProfileView: View {
             }
             .navigationTitle("회원정보 수정")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                nickname = userStore.currentUser?.nickName ?? ""
+                phoneNumber = userStore.currentUser?.phoneNumber ?? ""
+            }
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button(action: { isEditAlert.toggle() }, label: {
@@ -89,7 +127,7 @@ struct EditProfileView: View {
                         isEditAlert = false
                         dismiss()
                         Task {
-                            try await viewModel.updateUserData()
+                            try await userStore.updateUserData(image: selectedImage ,nick: nickname ,phone: phoneNumber)
                         }
                     })
                 )
